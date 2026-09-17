c9a2bdebe019b105f7143b7588d64b9570799941
success-ielts <successieltsexam@gmail.com>
2026-09-17 22:52:31 +0300

    feat: route production learning and recovery pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { RouteApp } from './LearningRoutes';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouteApp App={App} />
   </StrictMode>
 );
